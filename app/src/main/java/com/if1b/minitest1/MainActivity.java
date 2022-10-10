9bc60201e0ea275d25f7be2798dd8b7e19e6b76b
YandD9 <rifqivirgiansyah@gmail.com>
2022-10-10 10:58:34 +0700

    update1 MiniTest

--- app/src/main/java/com/if1b/minitest1/MainActivity.java
+++ app/src/main/java/com/if1b/minitest1/MainActivity.java
@@ -3,12 +3,26 @@ package com.if1b.minitest1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnKlik;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnKlik = findViewById(R.id.btn_klik);
+
+        btnKlik.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Apa Kabar Kamu", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
